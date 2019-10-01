23a5596c4f50d4e7fedb7b3c89952acf0ac70636
Troy van der Velden <326128@student.mboutrecht.nl>
2019-10-01 11:42:59 +0200

    Initial Commit

---
+++ ts/app.ts
@@ -0,0 +1,179 @@
+function troy(): void {
+
+    let a_tag: HTMLAnchorElement;
+    let p_tag: HTMLParagraphElement;
+    let h5_tag: HTMLHeadingElement;
+    let img_tag: HTMLImageElement;
+
+
+    a_tag = document.querySelector("#a_01");
+    p_tag = document.querySelector("#p_01");
+    h5_tag = document.querySelector("#h5_01");
+    img_tag = document.querySelector("#img_01");
+
+
+    // alert(p_tag.innerHTML);
+
+
+    if (a_tag.innerHTML == "Show other info") {
+        a_tag.innerHTML = "Return previous info";
+        p_tag.innerHTML = "This account is known for finding glitches in 'Pokémon Brick Bronze'.";
+        h5_tag.innerHTML = "Glitchlord"
+        img_tag.setAttribute("src","https://tr.rbxcdn.com/5f347eaec6e3642bdcf9f52a6390fa8f/352/352/Avatar/Png");
+    } else {
+        a_tag.innerHTML = "Show other info";
+        p_tag.innerHTML = "This is the first account I ever used to play Roblox on.";
+        h5_tag.innerHTML = "Troyvdv_1"
+        img_tag.setAttribute("src","https://tr.rbxcdn.com/2d7dcca6fdac33e318595cd9143d59de/150/150/AvatarHeadshot/Png");
+    }
+}
+
+function copy(): void {
+
+    let a_tag: HTMLAnchorElement;
+    let p_tag: HTMLParagraphElement;
+    let h5_tag: HTMLHeadingElement;
+    let img_tag: HTMLImageElement;
+
+
+    a_tag = document.querySelector("#a_02");
+    p_tag = document.querySelector("#p_02");
+    h5_tag = document.querySelector("#h5_02");
+    img_tag = document.querySelector("#img_02");
+
+
+    // alert(p_tag.innerHTML);
+
+
+    if (a_tag.innerHTML == "Show other info") {
+        a_tag.innerHTML = "Return previous info";
+        p_tag.innerHTML = "This account is known for being a notable and nice myth.";
+        h5_tag.innerHTML = "The myth, the legend"
+        img_tag.setAttribute("src","https://t1.rbxcdn.com/1d52d25ed51a9ac1262346cd6aba14a9");
+    } else {
+        a_tag.innerHTML = "Show other info";
+        p_tag.innerHTML = "My myth account, and no, I will not Copy your ID.";
+        h5_tag.innerHTML = "CopyID"
+        img_tag.setAttribute("src","https://tr.rbxcdn.com/5a7400f992d54cc41b6375d007247a3c/150/150/AvatarHeadshot/Png");
+    }
+}
+
+function glitch(): void {
+
+    let a_tag: HTMLAnchorElement;
+    let p_tag: HTMLParagraphElement;
+    let h5_tag: HTMLHeadingElement;
+    let img_tag: HTMLImageElement;
+
+
+    a_tag = document.querySelector("#a_03");
+    p_tag = document.querySelector("#p_03");
+    h5_tag = document.querySelector("#h5_03");
+    img_tag = document.querySelector("#img_03");
+
+
+    // alert(p_tag.innerHTML);
+
+
+    if (a_tag.innerHTML == "Show other info") {
+        a_tag.innerHTML = "Return previous info";
+        p_tag.innerHTML = "Not really known by many people, but there are 3 of this account.";
+        h5_tag.innerHTML = "Troll"
+        img_tag.setAttribute("src","https://t5.rbxcdn.com/e2430ad778de1b5cea75e842ed9e9bcc");
+    } else {
+        a_tag.innerHTML = "Show other info";
+        p_tag.innerHTML = "I like to troll other people on this account.";
+        h5_tag.innerHTML = "Gl1tchBIox"
+        img_tag.setAttribute("src","https://tr.rbxcdn.com/f6cda13ddc5699d80d97e134e2058bb7/150/150/AvatarHeadshot/Png");
+    }
+}
+
+function souf(): void {
+
+    let a_tag: HTMLAnchorElement;
+    let p_tag: HTMLParagraphElement;
+    let h5_tag: HTMLHeadingElement;
+    let img_tag: HTMLImageElement;
+
+
+    a_tag = document.querySelector("#a_04");
+    p_tag = document.querySelector("#p_04");
+    h5_tag = document.querySelector("#h5_04");
+    img_tag = document.querySelector("#img_04");
+
+
+    // alert(p_tag.innerHTML);
+
+
+    if (a_tag.innerHTML == "Show other info") {
+        a_tag.innerHTML = "Return previous info";
+        p_tag.innerHTML = "I met him on 'Pokémon Brick Bronze', we became great friends. he even became a myth friend.";
+        h5_tag.innerHTML = "SoUrNice"
+        img_tag.setAttribute("src","https://t4.rbxcdn.com/cdc946fb7ee6f816bdc084bd3ac48a0c");
+    } else {
+        a_tag.innerHTML = "Show other info";
+        p_tag.innerHTML = "A great friend to be a friend, my number one best friend.";
+        h5_tag.innerHTML = "SoufianeG"
+        img_tag.setAttribute("src","https://tr.rbxcdn.com/40ec4eb9e433d3860e1998384452ca5d/150/150/AvatarHeadshot/Png");
+    }
+}
+
+function kwelly(): void {
+
+    let a_tag: HTMLAnchorElement;
+    let p_tag: HTMLParagraphElement;
+    let h5_tag: HTMLHeadingElement;
+    let img_tag: HTMLImageElement;
+
+
+    a_tag = document.querySelector("#a_05");
+    p_tag = document.querySelector("#p_05");
+    h5_tag = document.querySelector("#h5_05");
+    img_tag = document.querySelector("#img_05");
+
+
+    // alert(p_tag.innerHTML);
+
+
+    if (a_tag.innerHTML == "Show other info") {
+        a_tag.innerHTML = "Return previous info";
+        p_tag.innerHTML = "A nice friend I met in 'Pokémon Brick Bronze', he makes glitching video's.";
+        h5_tag.innerHTML = "Sub to Kwelly"
+        img_tag.setAttribute("src","https://t6.rbxcdn.com/14174113cd94ff8b7daf2c07c8bef6d7");
+    } else {
+        a_tag.innerHTML = "Show other info";
+        p_tag.innerHTML = "A nice friend and glitcher, my second best friend.";
+        h5_tag.innerHTML = "Kwellyman1"
+        img_tag.setAttribute("src","https://tr.rbxcdn.com/3402a3826d210e9e86f563458dbf0763/150/150/AvatarHeadshot/Png");
+    }
+}
+
+function nano(): void {
+
+    let a_tag: HTMLAnchorElement;
+    let p_tag: HTMLParagraphElement;
+    let h5_tag: HTMLHeadingElement;
+    let img_tag: HTMLImageElement;
+
+
+    a_tag = document.querySelector("#a_06");
+    p_tag = document.querySelector("#p_06");
+    h5_tag = document.querySelector("#h5_06");
+    img_tag = document.querySelector("#img_06");
+
+
+    // alert(p_tag.innerHTML);
+
+
+    if (a_tag.innerHTML == "Show other info") {
+        a_tag.innerHTML = "Return previous info";
+        p_tag.innerHTML = "I met him on my myth acc, we have worked together on multiple myth games.";
+        h5_tag.innerHTML = "Good initiative"
+        img_tag.setAttribute("src","https://tr.rbxcdn.com/7443840de7469a609e13d7451454917e/352/352/Avatar/Png");
+    } else {
+        a_tag.innerHTML = "Show other info";
+        p_tag.innerHTML = "A nice friends to hang out with, my third best friend.";
+        h5_tag.innerHTML = "iiNanoRBLX"
+        img_tag.setAttribute("src","https://tr.rbxcdn.com/627163df7165c3034992854ef23a61e2/150/150/AvatarHeadshot/Png");
+    }
+}
